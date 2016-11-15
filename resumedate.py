1ac1192041145e9ceaae51b8aff3e9fd2268335b
M. Henry Linder <mhlinder@gmail.com>
2016-11-14 23:52:16 -0500

    final before bed

--- resumedate.py
+++ resumedate.py
@@ -1,26 +1,16 @@
-# webdir = '/home/mhlinder/www/'
-webdir = '.'
-
-f = open(webdir + 'resume.html', 'r')
-text = f.read()
-f.close()
 
 import datetime
-from re import compile
+from string import Template
+
+with open('resume.tmp', 'r') as f:
+    s = Template(f.read())
 
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 
-g1 = 'https://github.com/mhlinder">'
-g2 = '</a>'
-q = compile('(' + g1 + ')[A-Z 0-9]*(' + g2 + ')')
-text = q.sub(g1 + tomorrow.strftime("%d %b %y").upper() + g2, text)
+s = s.substitute(tomorrow = tomorrow.strftime("%d %b %y").upper(),
+                 today = today.strftime("%d %b %y").upper())
 
-g1 = 'https://twitter.com/mhlinder">'
-g2 = '</a>'
-q = compile('(' + g1 + ')[A-Z 0-9]*(' + g2 + ')')
-text = q.sub(g1 + today.strftime("%d %b %y").upper() + g2, text)
-
-f = open(webdir + 'resume.html', 'w')
-f.write(text)
-f.close()
+with open('resume2.html', 'w') as f:
+    f.write(s)
+    
