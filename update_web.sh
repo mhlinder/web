c5b0905b5f7b16dde9fdc8eb789e968765634443
M. Henry Linder <mhlinder@gmail.com>
2016-12-04 10:49:04 -0500

    updates web sync script

--- update_web.sh
+++ update_web.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd ~/research/web
-./resumedate.py
 ./music.py
-scp index.html schema.css resume.html music.html mhlinder@mhlinder.com:~/public_html
+scp index.php schema.css resume.php music.php mhlinder@mhlinder.com:~/public_html
 
