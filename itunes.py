c10a9acf5f83f5d138b3c428e8589384fe40647f
M. Henry Linder <mhlinder@gmail.com>
2016-12-04 20:52:56 -0500

    saves a markdown file containing all playlists' contents

--- itunes.py
+++ itunes.py
@@ -1,8 +1,21 @@
 
+from collections import defaultdict
 from pyItunes import Library
 
+# Get playlist names, given a list of IDs
 def get_names(ids):
     return([lookup[x]['name'] for x in ids])
+
+# Get direct parent folder, given a list of IDs
+def get_parent(ids):
+    return([lookup[x]['parents'][0] for x in ids])
+
+# Format a song based on title and artist
+def format_song(song):
+    if song.artist is None:
+        return("'{0}'".format(song.name))
+    else:
+        return("'{0}' - {1}".format(song.name, song.artist))
 
 l = Library('x/iTunes Music Library.xml')
 playlists = [x for x in l.getPlaylistNames() if x != 'iTunes\xa0U']
@@ -10,13 +23,12 @@ playlists = [x for x in l.getPlaylistNames() if x != 'iTunes\xa0U']
 # Key is playlist persistent ID
 lookup = dict()
 name_lookup = dict()
-# Root playlists / folders
-roots = list()
-# List of IDs whose parent does not exist when they are initially read
-parentless = list()
 
-# Format playlist entry lookup, especially with IDs connecting
-# children playlists to parents
+lounge = list()
+numbers = list()
+
+# Populates playlist entres in `lookup` by ID, which can be obtained
+# from a playlist name using `name_lookup`.
 for pn in playlists:
     p = l.getPlaylist(pn)
 
@@ -24,42 +36,60 @@ for pn in playlists:
     pid = p.parent_persistent_id
     name = p.name
 
-    lookup[cid] = { 'playlist' : p,
-                    'name'     : name,
-                    'id'       : cid,      # Current ID
-                    'pid'      : pid,      # Parent ID
-                    'children' : list() }
+    pp = { 'playlist' : p,
+           'name'     : name,
+           'id'       : cid,      # Current ID
+           'pid'      : pid,      # Parent ID
+           'children' : list() }
 
     name_lookup[name] = cid
 
-    if pid is None:
-        roots.append(cid)
-    else:
-        if pid not in lookup.keys():
-            print('error')
-            parentless.append(cid)
-        else:
-            lookup[pid]['children'].append(cid)
+    # Register as a child of a parent
+    if pid in lookup.keys():
+        lookup[pid]['children'].append(cid)
             
-for pn in playlists:
-    cid = name_lookup[pn]
-    p = lookup[cid]
-    pid = p['pid']
+    # Identify list of parent nodes, in order, until the root node
     parents = list()
     while pid is not None:
         parent = lookup[pid]
         parents.append(parent['name'])
         pid = parent['pid']
-    if len(parents) > 0:
-        lookup[cid]['parents'] =  parents
-        lookup[cid]['root'] =  parents[-1]
-    else:
-        lookup[cid]['parents'] = None
-        lookup[cid]['root'] = None
 
-for pn in playlists:
-    cid = name_lookup[pn]
-    p = lookup[cid]
-    if not p['playlist'].is_folder:
-        print(p['root'])
+    if len(parents) == 0:
+        pp['parents'] = None
+        pp['root'] = None
+    else:
+        pp['parents'] =  parents
+        root = parents[-1]
+        pp['root'] = root
+
+        if not p.is_folder:
+            if root == 'lounge':
+                lounge.append(cid)
+            elif root == 'numerology':
+                numbers.append(cid)
+
+    lookup[cid] = pp
+
+numbers_pl = defaultdict(dict)
+for cid in numbers:
+    pl = lookup[cid]
+    p = pl['playlist']
+
+    songs = [format_song(s) for s in p.tracks]
+    parent = pl['parents'][0]
+
+    numbers_pl[parent][p.name] = songs
+
+seasons = sorted(numbers_pl.keys())
+with open('x/playlists.md', 'w') as f:
+    for season in seasons:
+        f.write('\n\n# {0}'.format(season))
+        playlists = numbers_pl[season]
+        for pn in sorted(playlists.keys()):
+            songs = playlists[pn]
+            p_txt = '\n'.join(['* {0}'.format(s) for s in songs])
+            f.write('\n\n\n## {0}\n\n{1}'.format(pn, p_txt))
+
+    f.write('\n\n')
 
