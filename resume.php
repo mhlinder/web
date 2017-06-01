4eea91982996b7185bf8fed223b5a0b44a6df298
Henry Linder <mhlinder@gmail.com>
2017-06-01 07:02:21 -0700

    updates resume

--- resume.php
+++ resume.php
@@ -8,53 +8,59 @@
 
 <body>
 
-<div class="main mono">
-    [<a href="index">M. HENRY LINDER</a>]
-    <br />
-    <br />
+    <div class="main mono">
+        [<a href="index">M. HENRY LINDER</a>]
+        <br />
+        <br />
 
-    [<a href="https://twitter.com/mhlinder"><?php $tomorrow = new DateTime('tomorrow'); echo strtoupper($tomorrow->format('d M y')); ?></a>]
+        [<a href="https://twitter.com/mhlinder"><?php $tomorrow = new DateTime('tomorrow'); echo strtoupper($tomorrow->format('d M y')); ?></a>]
 
-    <br />
-    <br />
+        <br />
+        <br />
 
-    [09 JAN 17 -<br />
-    &nbsp;<?php echo strtoupper(date('d M y')); ?>&nbsp;&nbsp;&nbsp;JAX]
+        [30 APR 17 -<br />
+        &nbsp;<?php echo strtoupper(date('d M y')); ?>&nbsp;APPLE]
+
+        <br />
+        <br />
+
+        [09 JAN 17 -<br />
+        &nbsp;<?php echo strtoupper(date('d M y')); ?>&nbsp;&nbsp;&nbsp;JAX]
 
 
-    <br />
-    <br />
+        <br />
+        <br />
 
-    [25 AUG 14 -<br />
-    &nbsp;<?php echo strtoupper(date('d M y')); ?>&nbsp;UCONN]
+        [25 AUG 14 -<br />
+        &nbsp;<?php echo strtoupper(date('d M y')); ?>&nbsp;UCONN]
 
 
-    <br />
-    <br />
+        <br />
+        <br />
 
-    [19 MAY 14 -
+        [19 MAY 14 -
         <br />
         &nbsp;15 AUG 14&nbsp;&nbsp;&nbsp;OCR]
 
-    <br />
-    <br />
+        <br />
+        <br />
 
-    [22 JUN 12 -<br />
-     &nbsp;16 MAY 14 FRBNY]
+        [22 JUN 12 -<br />
+        &nbsp;16 MAY 14 FRBNY]
 
-    <br />
-    <br />
+        <br />
+        <br />
 
-    [27 MAY 12
-    <br />
-    &nbsp;SWARTHMORE COL.]
+        [27 MAY 12
+        <br />
+        &nbsp;SWARTHMORE COL.]
 
-    <br />
-    <br />
+        <br />
+        <br />
 
-    [15 AUG 90]
+        [15 AUG 90]
 
-</div>
+    </div>
 
 </body>
 
